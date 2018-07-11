9661e239827fb1a49ca59c5c50d23a0bed054ec0
groyce <groyce@unomaha.edu>
2018-07-11 15:49:40 -0500

    added pip install pprint note

--- rest/weather.py
+++ rest/weather.py
@@ -1,6 +1,7 @@
 import urllib.parse
 import requests
 from pprint import pprint
+#pip install pprint
 
 city = input('Enter City: ')
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=YOUR-KEY-HERE&units=metric'.format(city)
